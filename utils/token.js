14829a7b3df33db30c26f9bced2c689b39ce14ba
pavansannalaXTS <pavansannala@xtsservices.com>
2025-07-04 02:10:45 +0530

    token expry changes

--- utils/token.js
+++ utils/token.js
@@ -5,11 +5,11 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 const generateAccessToken = (payload) => {
-  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: '10h' });
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: '90d' });
 };
 
 const generateRefreshToken = (payload) => {
-  return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: '15d' });
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: '90d' });
 };
 
 module.exports = {
