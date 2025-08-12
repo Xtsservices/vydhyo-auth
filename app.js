32971a2bf65b1a7538a94af14bb6b0e8b9d4ac45
Mkp991203 <manikumarkuyyeti@gmail.com>
2025-08-12 22:42:13 +0530

    cors

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
 const bodyParser = require("body-parser");
+const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./utils/db');
 const logger = require('./utils/logger'); 
 
 // Middleware
 const app = express();
+
+const corsOptions = {
+  origin: ["https://vydhyo.com", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
